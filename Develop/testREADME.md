0336f0e7dee654cc52a78e211cbcd38fdd1de3d3
CheaC <cheacliatt@gmail.com>
2020-06-25 20:37:50 -0400

    It works! Sort of. Added questions, linked the codes.

---
+++ Develop/testREADME.md
@@ -0,0 +1,22 @@
+# laskdjf
+    # alksdjf
+    # Table of Contents
+    *[Installation](#installation) 
+    Usage
+    # License
+    # Contributing
+    # Tests
+    # Questions
+    Badges
+    # Installation Instructions
+    sdklfjs
+    # Usage
+    sdf
+    # Contrubutors
+    undefined
+    # Test
+    sdf
+    # Questions
+    If you have any questions, please contact undefined.
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,32 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    # ${data.description}
+    # Table of Contents
+    *[Installation](#installation) 
+    Usage
+    # License
+    # Contributing
+    # Tests
+    # Questions
+    Badges
+    # Installation Instructions
+    ${data.install}
+    # Usage
+    ${data.usage}
+    # Contrubutors
+    ${data.contrubution}
+    # Test
+    ${data.test}
+    # Questions
+    If you have any questions, please contact ${data.email}.
+
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
